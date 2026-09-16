221afc8bf51f4197418891ff404963c7a586cbd2
thanuja987 <veerathanuja987@gmail.com>
2026-09-16 23:05:03 +0530

    Create PalindromeNumber.java

---
+++ Math/PalindromeNumber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int orig=x;
+        if(x<0){
+            return false;
+        }
+       while(x>0){
+        int lastdigit=x%10;
+         rev=(rev*10)+lastdigit;
+         x=x/10;
+         
+       }
+       return orig==rev;
+       
+    }
+}
